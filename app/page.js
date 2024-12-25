cae3b996e46f31f8fad4adb5e151b318ae52ed99
Saksham Jaiswal <sakshamjaiswalofficial@gmail.com>
2024-12-25 14:57:15 +0530

    Initial Commit

--- app/page.js
+++ app/page.js
@@ -1,101 +1,27 @@
-import Image from "next/image";
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="h-screen flex flex-wrap pt-28">
+      <div className='binary-code z-0'></div>
+      
+      {/* <div className='flex flex-wrap md:flex-row flex-row-reverse'> */}
+      <div className='flex flex-wrap-reverse'>
+        <div className='md:w-1/2 w-full flex flex-col justify-center items-start p-10 z-10'>
+        <h1 className='text-7xl my-4 title'>Mohan Ganesh</h1>
+        <h2 className='text-3xl my-2 title2'>Cybersecurity Enthusiast</h2>
+        <p className='text-base my-2'>With a deep-rooted passion for cybersecurity, I specialize in identifying vulnerabilities, implementing effective defenses, and developing strategies to combat the ever-growing landscape of cyber threats. As a cybersecurity enthusiast, I focus on ethical hacking, penetration testing, and security architecture, always staying ahead of the curve in securing digital assets. My goal is to empower businesses and individuals with the knowledge and tools necessary to protect their data and infrastructure, ensuring a safer digital world for everyone.</p>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+        <div className='flex flex-wrap justify-start items-center w-full my-1'>
+          <Link href="/Contact" className='m-4 text-xl border p-3 rounded-xl button'>Contact Me</Link>
+          <Link href="/Experience" className='m-4 text-xl border p-3 rounded-xl button2'>Experience</Link>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        </div>
+
+        <div className='md:w-1/2 w-full flex justify-center items-center z-10'>
+          <img src="https://mohan-ganesh-babu-pothala.vercel.app/1709796195119.jpeg" alt="" className='h-[400px] w-[400px] rounded-full transform transition-transform duration-300 hover:scale-110'/>
+        </div>
+      </div>
     </div>
   );
 }
